1958871e31d544a17efdb30c65ebc83799363b4b
Fombi-Favour <fombifavour@gmail.com>
2023-07-10 17:51:59 +0100

    Update TodoApp.jsx

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -3,10 +3,12 @@ import Header from './Header';
 import TodosLogic from './TodosLogic';
 
 const TodoApp = () => (
-  <>
-    <Header />
-    <TodosLogic />
-  </>
+  <div className="wrapper">
+    <div className="todos">
+      <Header />
+      <TodosLogic />
+    </div>
+  </div>
 );
 
 export default TodoApp;
