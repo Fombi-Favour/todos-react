a81738e2b85e1e6a6741970e58d098ecf5e3aaa8
Fombi-Favour <fombifavour@gmail.com>
2023-07-11 12:50:31 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable no-nested-ternary */
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { useAuthContext } from '../context/AuthContext';
 
 const links = [
   { path: '/', text: 'Home' },
@@ -8,21 +10,52 @@ const links = [
   { path: 'login', text: 'Login' },
 ];
 
-const Navbar = () => (
-  <nav className="navbar">
-    <ul>
-      {links.map((link) => (
-        <li key={link.text}>
-          <NavLink
-            to={link.path}
-            className={({ isActive }) => (isActive ? 'active_class' : undefined)}
-          >
-            {link.text}
-          </NavLink>
-        </li>
-      ))}
-    </ul>
-  </nav>
-);
+const Navbar = () => {
+  const { user, logout } = useAuthContext();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate('/login');
+  };
+
+  return (
+    <>
+      <nav className="navbar">
+        <ul>
+          {links.map((link) => (
+            <React.Fragment key={link.text}>
+              {link.path === 'login' ? (
+                !user && (
+                <li>
+                  <NavLink to={link.path}>{link.text}</NavLink>
+                </li>
+                )
+              ) : link.path === 'profile' ? (
+                user && (
+                <li>
+                  <NavLink to={link.path}>
+                    {link.text}
+                  </NavLink>
+                </li>
+                )
+              ) : (
+                <li>
+                  <NavLink to={link.path}>{link.text}</NavLink>
+                </li>
+              )}
+            </React.Fragment>
+          ))}
+        </ul>
+      </nav>
+      {user && (
+      <div className="logout">
+        <p>{user}</p>
+        <button type="button" onClick={handleLogout}>Logout</button>
+      </div>
+      )}
+    </>
+  );
+};
 
 export default Navbar;
