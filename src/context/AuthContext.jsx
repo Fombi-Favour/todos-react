44230212774cd42ecce47a8a040beef62643f8f5
Fombi-Favour <fombifavour@gmail.com>
2023-07-11 12:57:30 +0100

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,11 +1,27 @@
-import { useState, useContext, createContext } from 'react';
+import {
+  useState, useEffect, useContext, createContext,
+} from 'react';
 
 const AuthContext = createContext(null);
 // eslint-disable-next-line react/prop-types
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState('');
+  function getUsername() {
+    // getting stored state
+    const temp = localStorage.getItem('username');
+    const savedUsername = JSON.parse(temp);
+    return savedUsername || '';
+  }
+
+  const [user, setUser] = useState(getUsername());
   const login = (user) => setUser(user);
   const logout = () => setUser(null);
+
+  useEffect(() => {
+    // storing user state
+    const temp = JSON.stringify(user);
+    localStorage.setItem('username', temp);
+  }, [user]);
+
   return (
     <AuthContext.Provider value={{ user, login, logout }}>
       {children}
