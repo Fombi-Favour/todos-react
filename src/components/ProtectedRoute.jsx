7e300cf0060f0b017dafa273a6d6c7f659ed0db8
Fombi-Favour <fombifavour@gmail.com>
2023-07-11 12:57:24 +0100

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -5,7 +5,6 @@ import { useAuthContext } from '../context/AuthContext';
 const ProtectedRoute = ({ children }) => {
   const { user } = useAuthContext();
   const location = useLocation();
-  console.log(location);
   if (!user) {
     return <Navigate to="/login" state={{ pathname: location.pathname }} replace />;
   }
